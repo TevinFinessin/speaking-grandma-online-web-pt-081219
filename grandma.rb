9015feeea562631c178e4900ec9a6b80cb0a6898
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-15 20:56:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ elsif phrase == phrase.downcase
   elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
 else
-  return "NO, NOT SINCE 1938!"
+  return "HUH?! SPEAK UP, SONNY!"
 # NO, NOT SINCE 1938!
 end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
