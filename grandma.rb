d17dfa9efc7f216b90c77041adc48f556b8c0896
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-15 20:27:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,8 @@ if phrase == "I LOVE YOU TOO GRANDMA!"
   return "I LOVE YOU TO PUMPKIN!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-
+elsif phrase == phrase.downcase
+return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
