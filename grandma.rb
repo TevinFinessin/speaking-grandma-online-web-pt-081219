7269ee8c0a494db63ecae924979bdfdbe7540416
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-15 20:25:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if phrase = "I LOVE YOU TOO GRANDMA!"
+if phrase == "I LOVE YOU TOO GRANDMA!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
