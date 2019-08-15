34dcf4d5a41f5fce2fc62a7100d318c03415f620
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-15 20:21:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,6 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
-if speak_to_grandma == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+def speak_to_grandma(phrase)
+
 end
 
 # Whatever you say to grandma, she should respond with
