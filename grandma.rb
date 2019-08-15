3f862063d521501dd4fc17eb62d2ca5737afd327
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-15 20:54:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@ return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.downcase
   return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
-  return 
+  return "NO, NOT SINCE 1938!"
 else
   return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
