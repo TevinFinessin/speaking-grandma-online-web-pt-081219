560ef80ecd857d7cc31026f6339ef9112d7cf285
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-15 20:52:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,7 @@ if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-elsif phrase == phrase.upcase
+elsif phrase == phrase.downcase
 return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
