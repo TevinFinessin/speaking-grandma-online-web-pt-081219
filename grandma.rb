719cb1c00d51185cab65df1fb04d49d2521f2a4b
Tevin Sherman <tevinsherman@yahoo.com>
2019-08-15 20:51:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@ if phrase == "I LOVE YOU GRANDMA!"
 # and yells back
 elsif phrase == phrase.downcase
 return "HUH?! SPEAK UP, SONNY!"
-else phrase == phrase.downcase
+elseif phrase == phrase.downcase
   return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
